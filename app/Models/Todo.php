6359aadb5bf10eee03329997b905738776df549b
ahhhngel <2001101382@student.buksu.edu.ph>
2023-03-22 18:34:23 +0800

    From lab_1

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,7 +7,7 @@ class Todo{
     public static function all(){
         $product_milktea = [
             [
-                'lead'          =>  'anne kyoTEA',
+                'lead'          =>  'kyoTEA',
                 'coffee_image'  =>  'assets/img/3463681.png',
                 'desc'          =>  'Creamy and Bobalicious'
             ]
